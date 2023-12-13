7098a16927cf4ff8301069c7f13d231e2ef6ed3e
Arthur Jen <arthur.jen@commsor.com>
2023-12-13 12:06:18 -0800

    added people and profile routes

---
+++ app/people/page.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'People',
+}
+export default function People() {
+  return <div>People</div>
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Profile',
+}
+export default function Profile() {
+  return <div>Profile</div>
+}
