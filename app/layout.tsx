4835664b3fd2099bb6d094eb979c4dd0021d55ac
Arthur Jen <arthur.jen@commsor.com>
2023-12-13 12:48:20 -0800

    added auth0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,28 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: 'LX: %s',
-    default: 'LX', // a default is required when creating a template
+    template: "LX: %s",
+    default: "LX", // a default is required when creating a template
   },
-  description: 'Magic: the Gathering Card Lending app by Cherry City Gaming',
-}
+  description: "Magic: the Gathering Card Lending app by Cherry City Gaming",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <UserProvider>
+        <body className={inter.className}>{children}</body>
+      </UserProvider>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -41,8 +41,9 @@ export default function Home() {
           height={37}
           priority
         />
+        
       </div>
-
+      <a href="/api/auth/login">Login</a>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
