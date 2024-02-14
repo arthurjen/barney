9803f513f7b0db8129f1b3bfcce49d12786fc4c3
Arthur Jen <arthur.jen@commsor.com>
2024-02-14 13:40:32 -0800

    added styling to transactions

--- app/borrow/transactions-list.tsx
+++ app/borrow/transactions-list.tsx
@@ -7,19 +7,24 @@ export default function TransactionsList({
   transactions: Transaction[];
 }) {
   return transactions.map((transaction, index) => (
-    <div
-      key={index}
-      className="border-main bg-secondary border-2 flex justify-between items-center p-4"
-    >
-      <div className="border-main border-2 flex-none text-xl size-7 text-main text-center leading-none pt-[1px]">
-        {sumCardQuantities(transaction)}
+    <div key={index} className="relative z-0">
+      <div className="z-10 border-main bg-secondary border-2 flex justify-between items-center p-4 h-20">
+        <div className="border-main border-2 flex-none text-xl size-7 text-main text-center leading-none pt-[1px]">
+          {sumCardQuantities(transaction)}
+        </div>
+        <div className="text-main grow text-xl pl-4 truncate">
+          {truncateCardNames(transaction)}
+        </div>
+        <div className="flex-none">
+          <Image
+            src="placeholder.svg"
+            alt="profile pic"
+            width={36}
+            height={36}
+          />
+        </div>
       </div>
-      <div className="text-main grow text-xl pl-4 truncate">
-        {truncateCardNames(transaction)}
-      </div>
-      <div className="flex-none">
-        <Image src="placeholder.svg" alt="profile pic" width={36} height={36} />
-      </div>
+      <div className="absolute top-2 left-2 -z-10 bg-secondary border-main border-2 h-20 w-full" />
     </div>
   ));
 }
